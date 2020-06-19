acbdd17cd4a096bdb55f2cab7ab0e57a32cd9b30
taras_koloshmatin <taras.koloshmatin@gmail.com>
2020-06-19 22:18:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/music_app/presentation/RickMortyApplication.kt
@@ -0,0 +1,40 @@
+package com.example.music_app.presentation
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class MusicApplication : Application() {
+
+    companion object {
+        lateinit var sInstance: MusicApplication
+            private set
+
+        val getAppInstance: MusicApplication
+            get() = sInstance
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        sInstance = this
+        Stetho.initializeWithDefaults(this@MusicApplication)
+        Timber.plant(Timber.DebugTree())
+        setKoin()
+    }
+
+    private fun setKoin() = startKoin {
+        androidContext(this@MusicApplication)
+        modules(
+            listOf(
+//                viewModelModule,
+//                repositoryModule,
+//                networkModule,
+//                serviceModule,
+//                databaseModule,
+//                daoModule
+            )
+        )
+    }
+}
